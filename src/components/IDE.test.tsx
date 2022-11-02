012e03a0e21778ea8212eea04e9b7617f08136f4
trmttm <96355189+trmttm@users.noreply.github.com>
2022-11-02 12:56:19 +0400

    test Line properties match between state and screen

--- src/components/IDE.test.tsx
+++ src/components/IDE.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { IDE } from "./IDE";
 import * as setting from "../setting";
+import { getLineById } from "../setting";
 
 function getAddNewButton() {
   return screen.getByRole("button", { name: setting.btnTextAddNewAccount });
@@ -60,9 +61,27 @@ test("lines are drawn per Lines model", () => {
   render(<IDE />);
   testIfSpecifiedLineExists(setting.connectorLineId);
   testIfSpecifiedLineExists(0);
+
+  testLinePropertiesMatch(setting.connectorLineId);
+  testLinePropertiesMatch(0);
 });
 
 function testIfSpecifiedLineExists(lineId: string | number) {
   const connectorLine = screen.getByTestId(lineId);
   expect(connectorLine).toBeInTheDocument();
+}
+
+function testLinePropertiesMatch(lineId: number | string) {
+  const lineState = getLineById(lineId);
+  const lineElement = screen.getByTestId(lineId);
+  const expectedX = lineState !== undefined ? lineState.x + "px" : "0px";
+  const expectedY = lineState !== undefined ? lineState.y + "px" : "0px";
+  const expectedAngle =
+    lineState !== undefined ? lineState.angle + "deg" : "0deg";
+  const expectedLength =
+    lineState !== undefined ? lineState.width + "px" : "0px";
+  expect(lineElement.style.marginLeft).toBe(expectedX);
+  expect(lineElement.style.marginTop).toBe(expectedY);
+  expect(lineElement.style.rotate).toBe(expectedAngle);
+  expect(lineElement.style.width).toBe(expectedLength);
 }
--- src/setting.tsx
+++ src/setting.tsx
@@ -29,3 +29,13 @@ export const Lines: typeLine[] = [
   { id: connectorLineId, x: 10, y: 20, width: 30, angle: 30 },
   { id: 0, x: 10, y: 20, width: 30, angle: 30 },
 ];
+
+export function getLineById(id: string | number): typeLine | undefined {
+  let lineToReturn = undefined;
+  Lines.forEach((line) => {
+    if (line.id === id) {
+      lineToReturn = line;
+    }
+  });
+  return lineToReturn;
+}
